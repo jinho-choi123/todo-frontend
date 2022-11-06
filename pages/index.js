4d656a28621d3b27b2fffbc99ae7aece7a1d40b8
Jinho-Choi123 <choijinho817@gmail.com>
2022-11-06 10:34:19 +0000

    install nextjs, material ui

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button'
+
+const Home = () => {
+  return (
+    <div>
+        This is home page
+        <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default Home
