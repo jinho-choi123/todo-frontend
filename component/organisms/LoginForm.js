340a39ae9244c0e6418fbce61769e8d058a464a1
ball <choijinho817@gmail.com>
2022-11-14 15:37:45 +0000

    make login form

---
+++ component/organisms/LoginForm.js
@@ -0,0 +1,67 @@
+import { FormControl, InputLabel, Input, FilledInput, InputAdornment, IconButton, Box} from "@mui/material"
+import {useState} from 'react'
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+const LoginForm = () => {
+    const [values, setValues] = useState({
+        username: '',
+        password1: '',
+        password2: '',
+        showPassword: false,
+    })
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+      
+    const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault()
+    }
+
+    return (
+        <Box
+        component="form"
+        sx={{
+        '& > :not(style)': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+            <FormControl>
+                <InputLabel>Username</InputLabel>
+                <Input value={values.username} onChange={handleChange('username')}></Input>
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                    id="standard-adornment-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        >
+                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                />
+            </FormControl>
+        </Box>
+    )
+
+}
+
+export default LoginForm
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,5 @@
-import { FormControl, InputLabel, Input } from "@mui/material"
-import { Box } from "@mui/system"
 import {useState} from 'react'
+import LoginForm from "../component/organisms/LoginForm";
 
 const Login = () => {
     const [values, setValues] = useState({
@@ -21,43 +20,12 @@ const Login = () => {
         });
       };
 
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault()
+    }
+
     return (
-        <Box
-        component="form"
-        sx={{
-        '& > :not(style)': { m: 1 },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-            Welcome to TODO APP
-            <FormControl>
-                <InputLabel htmlFor="component-simple">Username</InputLabel>
-                <Input value={values.username}></Input>
-            </FormControl>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        </Box>
+        <LoginForm/>
     )
 }
 
